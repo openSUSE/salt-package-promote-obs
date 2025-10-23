6b3b2f4307c636879e46177603b66517c9a215dd
Pablo Suárez Hernández <psuarezhernandez@suse.com>
2025-10-23 15:57:09 +0100

    Move common functions to scmutils.py

---
+++ scmutils.py
@@ -0,0 +1,126 @@
+#!/usr/bin/python3
+"""
+This file contains helper functions to interact with
+Git repositories and perform some operations
+"""
+
+import subprocess
+from typing import Dict, List
+
+import requests
+
+
+def fetch_repos_json(git_server: str, org: str) -> str:
+    """
+    Use gitea API to fetch the list of repositories for a given organization.
+    """
+    output = []
+    keep_fetching = True
+    page = 1
+
+    while keep_fetching:
+        ret = requests.get(
+            f"https://{git_server}/api/v1/users/{org}/repos?limit=100&page={page}"
+        ).json()
+        if not ret:
+            keep_fetching = False
+        else:
+            page += 1
+            output.extend(ret)
+
+    return output
+
+
+def get_commit_hash(
+    git_server: str, org: str, repo_name: str, branch: str, headers: Dict = {}
+) -> str:
+    """
+    Get latest commit hash for a given branch name
+    """
+    ret = None
+    ret = requests.get(
+        f"https://{git_server}/api/v1/repos/{org}/{repo_name}/branches/{branch}",
+        headers=headers,
+    ).json()["commit"]["id"]
+    return ret
+
+
+def run_git(command: str, cwd: str = None):
+    """
+    Run a git command
+    """
+    _cmd = f"git {command}"
+    result = subprocess.run(
+        _cmd,
+        shell=True,
+        check=True,
+        stdout=subprocess.PIPE,
+        stderr=subprocess.PIPE,
+        universal_newlines=True,
+        cwd=cwd,
+    )
+    return result
+
+
+def promote_package(
+    git_server: str,
+    org: str,
+    repo_name: str,
+    source_branch: str,
+    target_branch: str,
+    auth_token: str,
+    cwd: str,
+):
+    """
+    Promote SOURCE_BRANCH to TARGET_BRANCH
+    """
+    pass
+    # run_git("init --bare --object-format=sha256", cwd=cwd)
+    # run_git(
+    #    f"remote add source https://{git_server}/{org}/{repo_name}",
+    #    cwd=cwd,
+    # )
+    # run_git(
+    #   f"remote add target https://{auth_token}@{target_git_server}/{target_org}/{repo_name}",
+    #   cwd=cwd,
+    # )
+    # run_git(f"fetch source {source_branch}:{source_branch}", cwd=cwd)
+    # run_git(f"push target {source_branch}:{target_branch}", cwd=cwd)
+
+
+def sync_branches_for_repo(
+    source_git_server: str,
+    source_org: str,
+    repo_name: str,
+    source_branch: str,
+    target_git_server: str,
+    target_org: str,
+    target_branches: List[str],
+    cwd: str,
+):
+    """
+    Synchronize target_branches according to source_branch
+    """
+    pass
+    # run_git("init --bare --object-format=sha256", cwd=cwd)
+    # run_git(
+    #   f"remote add source https://{source_git_server}/{source_org}/{repo_name}",
+    #   cwd=cwd,
+    # )
+    # run_git(
+    #   f"remote add target https://{auth_token}@{target_git_server}/{target_org}/{repo_name}",
+    #   cwd=cwd,
+    # )
+    # run_git(f"fetch source {source_branch}:{source_branch}", cwd=cwd)
+    # for tgt in target_branches:
+    #   run_git(f"push target {source_branch}:{tgt}", cwd=cwd)
+
+
+def get_repo_list(git_server: str, org: str, exclude: List[str] = None) -> List[str]:
+    """
+    Returns the list of repository names to process without excluded repositories
+    """
+    if not exclude:
+        exclude = []
+    repos_json = fetch_repos_json(git_server, org)
+    return [repo["name"] for repo in repos_json if repo["name"] not in exclude]
--- sync_saltbundle_packages.py
+++ sync_saltbundle_packages.py
@@ -4,29 +4,27 @@ This script takes care of the automation to keep the
 packages from https://src.opensuse.org/saltbundle/ in sync
 with the packages at https://src.suse.de/Galaxy/
 
-The SOURCE_BRANCH for each package (at SOURCE_GIT_REPO/SOURCE_GIT_ORG)
-will be pushed to TARGET_BRANCHES (at TARGET_GIT_REPO/TARGET_GIT_ORG).
+The SOURCE_BRANCH for each package (at SOURCE_GIT_SERVER/SOURCE_GIT_ORG)
+will be pushed to TARGET_BRANCHES (at TARGET_GIT_SERVER/TARGET_GIT_ORG).
 
-An access token for TARGET_GIT_REPO/TARGET_GIT_ORG is required
+An access token for TARGET_GIT_SERVER/TARGET_GIT_ORG is required
 with the following permissions:
   - "repository/package/organization": read/write
   - "user": read only
 """
 
-import json
 import os
 import subprocess
 import sys
 import tempfile
-from typing import Dict, List
 
-import requests
+import scmutils
 
-SOURCE_GIT_REPO = "src.opensuse.org"
+SOURCE_GIT_SERVER = "src.opensuse.org"
 SOURCE_GIT_ORG = "saltbundle"
 SOURCE_BRANCH = "bundle"
 
-TARGET_GIT_REPO = "src.suse.de"
+TARGET_GIT_SERVER = "src.suse.de"
 TARGET_GIT_ORG = "Galaxy"
 TARGET_BRANCHES = ["mlmtools-main", "mlmtools-stable"]
 
@@ -36,129 +34,73 @@ AUTH_HEADERS = {"Authorization": f"Bearer {TARGET_REPO_TOKEN}"}
 REPOS_TO_EXCLUDE = ["_ObsPrj"]
 
 
-def _fetch_repos_json(git_repo: str, org: str) -> str:
-    """
-    Use gitea API to fetch the list of repositories for a given organization.
-    """
-    output = []
-    keep_fetching = True
-    page = 1
-
-    while keep_fetching:
-        ret = requests.get(
-            f"https://{git_repo}/api/v1/users/{org}/repos?limit=100&page={page}"
-        ).json()
-        if not ret:
-            keep_fetching = False
-        else:
-            page += 1
-            output.extend(ret)
-
-    return output
-
-
-def _get_commit_hash(
-    git_repo: str, org: str, repo_name: str, branch: str, headers: Dict = {}
-) -> str:
-    """
-    Get latest commit hash for a given branch name
-    """
-    ret = None
-    ret = requests.get(
-        f"https://{git_repo}/api/v1/repos/{org}/{repo_name}/branches/{branch}",
-        headers=headers,
-    ).json()["commit"]["id"]
-    return ret
-
-
-def _run_git(command: str, cwd: str = None):
-    """
-    Run a git command
-    """
-    _cmd = f"git {command}"
-    result = subprocess.run(
-        _cmd,
-        shell=True,
-        check=True,
-        stdout=subprocess.PIPE,
-        stderr=subprocess.PIPE,
-        universal_newlines=True,
-        cwd=cwd,
-    )
-    return result
-
-
-def _sync_branches_for_repo(repo_name: str, cwd: str):
-    """
-    Synchronize TARGET_BRANCHES according to SOURCE_BRANCH
-    """
-    _run_git("init --bare --object-format=sha256", cwd=cwd)
-    _run_git(
-        f"remote add source https://{SOURCE_GIT_REPO}/{SOURCE_GIT_ORG}/{repo_name}",
-        cwd=cwd,
-    )
-    _run_git(
-        f"remote add target https://{TARGET_REPO_TOKEN}@{TARGET_GIT_REPO}/{TARGET_GIT_ORG}/{repo_name}",
-        cwd=cwd,
-    )
-    _run_git(f"fetch source {SOURCE_BRANCH}:{SOURCE_BRANCH}", cwd=cwd)
-    for tgt in TARGET_BRANCHES:
-        _run_git(f"push target {SOURCE_BRANCH}:{tgt}", cwd=cwd)
-
-
-def get_repo_list(exclude: List[str]) -> List[str]:
-    """
-    Returns the list of repository names to process without excluded repositories
-    """
-    repos_json = _fetch_repos_json(SOURCE_GIT_REPO, SOURCE_GIT_ORG)
-    return [repo["name"] for repo in repos_json if repo["name"] not in REPOS_TO_EXCLUDE]
-
-
-stats = {"processed": 0, "synced": [], "errors": []}
-for repo in get_repo_list(REPOS_TO_EXCLUDE):
-    print(f"Processing package https://{SOURCE_GIT_REPO}/{SOURCE_GIT_ORG}/{repo} ...")
+stats = {"processed": 0, "synced": [], "to_sync": [], "errors": []}
+for repo in scmutils.get_repo_list(
+    git_server=SOURCE_GIT_SERVER, org=SOURCE_GIT_ORG, exclude=REPOS_TO_EXCLUDE
+):
+    print(f"Processing package https://{SOURCE_GIT_SERVER}/{SOURCE_GIT_ORG}/{repo} ...")
     stats["processed"] += 1
+    force_sync = False
 
     try:
-        source_hash = _get_commit_hash(
-            SOURCE_GIT_REPO, SOURCE_GIT_ORG, repo, SOURCE_BRANCH
+        source_hash = scmutils.get_commit_hash(
+            git_server=SOURCE_GIT_SERVER,
+            org=SOURCE_GIT_ORG,
+            repo_name=repo,
+            branch=SOURCE_BRANCH,
         )
+        print(f"---> HEAD ({SOURCE_BRANCH}): {source_hash}")
+        for tgt in TARGET_BRANCHES:
+            target_hash = scmutils.get_commit_hash(
+                git_server=TARGET_GIT_SERVER,
+                org=TARGET_GIT_ORG,
+                repo_name=repo,
+                branch=tgt,
+                headers=AUTH_HEADERS,
+            )
+            print(f"---> HEAD ({tgt}): {target_hash}")
+            if source_hash != target_hash:
+                print(f"---> YAY!!! We need to sync branch {tgt} here!")
+                force_sync = True
     except Exception:
         print("---> ERROR: cannot get commit hash. Check configured access token!")
+        print()
         stats["errors"].append(repo)
         continue
 
-    print(f"---> HEAD ({SOURCE_BRANCH}): {source_hash}")
-    force_sync = False
-    for tgt in TARGET_BRANCHES:
-        target_hash = _get_commit_hash(
-            TARGET_GIT_REPO, TARGET_GIT_ORG, repo, tgt, AUTH_HEADERS
-        )
-        print(f"---> HEAD ({tgt}): {target_hash}")
-        if source_hash != target_hash:
-            print(f"---> YAY!!! We need to sync branch {tgt} here!")
-            force_sync = True
     if force_sync:
         print(
-            f"---> A sync is needed here against https://{TARGET_GIT_REPO}/{TARGET_GIT_ORG}/{repo} !!!"
+            f"---> A sync is needed here against https://{TARGET_GIT_SERVER}/{TARGET_GIT_ORG}/{repo} !!!"
         )
+        stats["to_sync"].append(repo)
         try:
             with tempfile.TemporaryDirectory() as tmpdir:
                 try:
-                    _sync_branches_for_repo(repo, tmpdir)
+                    scmutils.sync_branches_for_repo(
+                        source_git_server=SOURCE_GIT_SERVER,
+                        source_org=SOURCE_GIT_ORG,
+                        repo_name=repo,
+                        source_branch=SOURCE_BRANCH,
+                        target_git_server=TARGET_GIT_SERVER,
+                        target_org=TARGET_GIT_ORG,
+                        target_branches=TARGET_BRANCHES,
+                        cwd=tmpdir,
+                    )
                     stats["synced"].append(repo)
                 except subprocess.CalledProcessError as exc:
                     print(f"   Git Command failed: {exc.cmd}")
                     print(f"   STDOUT: {exc.stdout}")
                     print(f"   STDERR: {exc.stderr}")
                     stats["errors"].append(repo)
+                    print()
                     continue
         except Exception as exc:
             print(f"---> ERROR: {exc}")
             stats["errors"].append(repo)
+            print()
             continue
         print(
-            f"---> Pushed branch '{SOURCE_BRANCH}' from {SOURCE_GIT_REPO} to branches '{TARGET_BRANCHES}' at {TARGET_GIT_REPO}"
+            f"---> Pushed branch '{SOURCE_BRANCH}' from {SOURCE_GIT_SERVER} to branches '{TARGET_BRANCHES}' at {TARGET_GIT_SERVER}"
         )
         print("---> Successfully synced!")
     else:
@@ -166,7 +108,13 @@ for repo in get_repo_list(REPOS_TO_EXCLUDE):
     print()
 
 print("----------------------------------------------------------------")
-print(f" Packages that need to be synced: ", end="")
+print(f" Total packages processed: {stats['processed']}")
+print(" Packages that required a sync: ", end="")
+if not stats["to_sync"]:
+    print("(none)")
+else:
+    print(len(stats["to_sync"]))
+print(" Packages that were successfully synced: ", end="")
 if not stats["synced"]:
     print("(none)")
 else:
@@ -176,8 +124,7 @@ else:
 if stats["errors"]:
     print(" Packages with errors:")
     for pkg in stats["errors"]:
-        print(f" * {pkg}")
-print(f" Total packages processed: {stats['processed']}")
+        print(f" * ERROR {pkg}")
 print("----------------------------------------------------------------")
 
 if stats["errors"]:
